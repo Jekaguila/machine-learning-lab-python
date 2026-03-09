1fe5051deed11fb30c60ca35644950d985e9982f
Jekaguila <doulus.jefis@gmail.com>
2026-03-08 20:38:54 -0600

    Add Random Forest classification model example

--- notebooks/03_classification_models.py
+++ notebooks/03_classification_models.py
@@ -1,1 +1,18 @@
+# Classification models
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+
+data = load_iris()
+
+X = data.data
+y = data.target
+
+model = RandomForestClassifier()
+
+model.fit(X, y)
+
+predictions = model.predict(X)
+
+print("Accuracy:", accuracy_score(y, predictions))
