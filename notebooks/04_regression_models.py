9748d5eb4dcbe838179fb803ad0f2e43bcb1a808
Jekaguila <doulus.jefis@gmail.com>
2026-03-08 20:39:15 -0600

    Add linear regression example in 04_regression_models.py
    Added a simple linear regression example using scikit-learn.

--- notebooks/04_regression_models.py
+++ notebooks/04_regression_models.py
@@ -1,1 +1,15 @@
+# Regression example
 
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+X = np.array([[1],[2],[3],[4],[5]])
+y = np.array([2,4,6,8,10])
+
+model = LinearRegression()
+
+model.fit(X,y)
+
+prediction = model.predict([[6]])
+
+print("Prediction:", prediction)
